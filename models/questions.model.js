b9ae9fef009ddb2dd565be913acbcacc398c686e
karimulla1696 <karimulla1696@gmail.com>
2019-09-14 23:16:37 +0530

    removed unnecessery files

--- models/questions.model.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const __questionsSchema = new mongoose.Schema({
-  Title: String,
-  Page: Number
-});
-
-module.exports = mongoose.model('Questions', __questionsSchema);
